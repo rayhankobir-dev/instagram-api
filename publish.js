be62c0d9dc23033a01de5d35aa9f0c737508486c
Rayhan Kobir <52678141+rajurayhan37@users.noreply.github.com>
2023-09-21 14:36:24 +0600

    Update publish.js

--- publish.js
+++ publish.js
@@ -1,14 +1,14 @@
 const { IgApiClient } = require("instagram-private-api");
 const fs = require("fs");
 
-const postToInsta = async () => {
+const postToInsta = async (data) => {
   try {
     const ig = new IgApiClient();
     ig.state.generateDevice(process.env.IG_USERNAME);
     await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
 
     await ig.publish.photo({
-      file: fs.readFileSync("./output.jpg"),
+      file: data,
     });
 
     return true;
